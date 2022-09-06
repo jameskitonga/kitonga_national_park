ad822670a18b4ca3701de6f2ecd06155e0b74808
jameskitonga <jameskitonga432@gmail.com>
2022-09-06 11:02:22 +0300

    added a change on the contactus page

--- script/app.js
+++ script/app.js
@@ -42,19 +42,22 @@ function validate_password() {
 // }
 
 
-$(function () {
-    $("#btnSubmit").click(function () {
-        var password = $("#txtPassword").val();
-        var confirmPassword = $("#txtConfirmPassword").val();
+
+
+
+
+
+const passWord=getElementById("password")
+
+    function Validate() {
+        var password = document.getElementById("txtPassword").value;
+        var confirmPassword = document.getElementById("txtConfirmPassword").value;
         if (password != confirmPassword) {
             alert("Passwords do not match.");
             return false;
         }
         return true;
-    });
-});
-
-
+    }
 
 
 
